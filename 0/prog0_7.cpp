207ae4ec90032e83ee46cf2d889d9d56cf003b70
Alex R. Mead <alex.r.mead@gmail.com>
2017-11-08 18:23:41 -0800

    Chapter 0 exercises

---
+++ 0/prog0_7.cpp
@@ -0,0 +1,10 @@
+// a small C++ program in which I'm playing with \t or the "tab" character
+#include <iostream>
+
+int main() 
+{
+	/* This is a comment that extends over several lines
+		because it uses /* and */ as its starting and ending delimiters */
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
