d7a61b551687eaeed30b008c62891e7bfa4a4917
Alex R. Mead <alex.r.mead@gmail.com>
2017-11-09 12:35:24 -0800

    Adding pg 12 code.

---
+++ ch1/pg12.cpp
@@ -0,0 +1,33 @@
+// ask for a person's name, and generate a framed greeting
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // ask for the person's name
+    std::cout << "Please enter your first name: ";
+    
+    // read the name
+    std::string name; // define name
+    std::cin >> name; // read into name
+    
+    // build the message that we intend to write
+    const std::string greeting = "Hello, " + name + "!";
+    
+    // build the 2nd and 4th lines of the output
+    const std::string spaces(greeting.size(), ' ');
+    const std::string second = "* " + spaces + " *";
+    
+    // build 1st and 5th lines
+    const std::string first(second.size(),'*');
+    
+    // write it all
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
+    
+    return 0;
+}
